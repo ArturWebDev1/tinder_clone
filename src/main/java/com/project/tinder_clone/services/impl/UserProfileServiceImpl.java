dba0e97bacfcc0577d49a281a183770a2c9c1b15
ArturWebDev1 <bboy.snip222@gmail.com>
2025-08-08 15:55:39 +0300

    Update profile method is added.

--- src/main/java/com/project/tinder_clone/services/impl/UserProfileServiceImpl.java
+++ src/main/java/com/project/tinder_clone/services/impl/UserProfileServiceImpl.java
@@ -3,10 +3,13 @@ package com.project.tinder_clone.services.impl;
 import com.project.tinder_clone.domain.entries.UserProfile;
 import com.project.tinder_clone.repositories.UserProfileRepository;
 import com.project.tinder_clone.services.UserProfileService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +31,25 @@ public class UserProfileServiceImpl implements UserProfileService {
         return userRepo.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User profile not found with id " + id));
 
+    }
+
+    @Transactional  // везде где больше чем один вызов репо, нужно добавлять для безопасности эту аннотацию
+    @Override
+    public UserProfile updateProfileById(Long id, UserProfile profile) {
+        // validation
+        if (null == profile.getName() || profile.getName().isBlank()) {
+            throw new IllegalArgumentException("A profile must have a name!");
+        }
+
+        // Changes
+        UserProfile existingProfile = userRepo.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("Task not found!");
+        });
+
+        existingProfile.setName(profile.getName());
+        existingProfile.setAge(profile.getAge());
+        existingProfile.setBio(profile.getBio());
+
+        return userRepo.save(existingProfile);
     }
 }
